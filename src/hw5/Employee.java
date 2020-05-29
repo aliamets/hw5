2b367d31089c53f42b0359ecee842bfd7b0b6671
Лямец Алексей <65312091+aliamets@users.noreply.github.com>
2020-05-29 10:25:27 +0300

    hw5

--- src/hw5/Employee.java
+++ src/hw5/Employee.java
@@ -8,5 +8,29 @@ public class Employee {
     private int salary;
     private int age;
 
+    public Employee(String fullName, String position, String eMail, int phone, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.eMail = eMail;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
 
+    public void info() {
+        System.out.print("ФИО:" + fullName + " Должность: "
+                + position + " Почта: " + eMail + " Телефон: " + phone + " Ожидаемый доход: " + salary
+                + " Возраст: " + age + ".");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge() {
+        if (age < 40) {
+            return;
+        }
+        this.age = age;
+    }
 }
--- src/hw5/Main.java
+++ src/hw5/Main.java
@@ -1,5 +1,26 @@
 package hw5;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee[] employee = new Employee[5];
+        employee[0] = new Employee("Василий Иванович", "Повар",
+                "VI@mail.ru", 330503, 30000, 55);
+        employee[1] = new Employee("Иван Васильевич", "Официант", "IV@mail.ru", 220002, 25000, 25);
+        employee[2] = new Employee("Алексей Петрович", "Официант",
+                "AP@mail.ru", 658742, 25000, 24);
+        employee[3] = new Employee("Мария Сергеевна", "Официант",
+                "MS@mail.ru", 985324, 25000, 28);
+        employee[4] = new Employee("Надежда Михайловна", "Администратор",
+                "NM@mail.ru", 285012, 60000, 48);
+        suitableStaff(employee);
+    }
 
+    public static void suitableStaff(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i].getAge() > 40) {
+                employee[i].info();
+                System.out.println();
+            }
+        }
+    }
 }
