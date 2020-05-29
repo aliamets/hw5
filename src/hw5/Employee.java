1696713960dbc6e1656a8b4c6d3da6834e423985
Лямец Алексей <65312091+aliamets@users.noreply.github.com>
2020-05-29 10:24:02 +0300

    Initial commit

---
+++ src/hw5/Employee.java
@@ -0,0 +1,12 @@
+package hw5;
+
+public class Employee {
+    private String fullName;
+    private String position;
+    private String eMail;
+    private int phone;
+    private int salary;
+    private int age;
+
+
+}
